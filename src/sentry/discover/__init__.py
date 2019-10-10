ce0ec1b15a009bbe2034c6908d5209d129a6757a
Joshua Li <josh@jrl.ninja>
2019-10-10 16:30:18 -0700

    move sentry.discover.tasks to CELERY_IMPORTS

--- src/sentry/discover/__init__.py
+++ src/sentry/discover/__init__.py
@@ -1,6 +1,1 @@
 from __future__ import absolute_import
-
-# XXX: had to unblock Django 1.9 AppRegistryNotReady due to eventual
-# model import starting from sentry.deletions.load_defaults()
-# Can this be outright removed?
-# from . import tasks  # NOQA
