78f702af8258c0c048b36fadbfd8b2d197dc04bb
Billy Vong <billy@sentry.io>
2019-10-14 11:45:33 -0700

    test(enzyme): Change setting for `disableLifecycleMethods` to be default (false)
    This behavior is confusing for people that would expect lifecycle methods to fire in tests.

--- tests/js/setup.js
+++ tests/js/setup.js
@@ -22,7 +22,6 @@ fromEntries.shim();
  * Enzyme configuration
  */
 Enzyme.configure({adapter: new Adapter()});
-Enzyme.configure({disableLifecycleMethods: true});
 
 /**
  * Mock (current) date to alway be below
